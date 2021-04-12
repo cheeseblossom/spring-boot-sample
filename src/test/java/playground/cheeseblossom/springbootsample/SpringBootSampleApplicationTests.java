67bc024b11855bf2c70748066021a7b4c2f70143
cheeseblossom <59395805+cheeseblossom@users.noreply.github.com>
2021-04-12 19:09:05 +0900

    /add initial project

---
+++ src/test/java/playground/cheeseblossom/springbootsample/SpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package playground.cheeseblossom.springbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
